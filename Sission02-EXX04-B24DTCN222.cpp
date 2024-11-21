0f946c494040767eff0309a6256f6de40af71909
hieunv-0908 <nn650370@gmail.com>
2024-11-21 16:11:12 +0700

    Add files via upload

---
+++ Sission02-EXX04-B24DTCN222.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int canh =5;
+	int S = canh * canh;
+	printf ("dien tich cua hinh vuong la %d\n", S);
+	int C = canh * 4;
+	printf ("chu vi cua hinh vuong la %d\n", C);
+	return 0;
+}
